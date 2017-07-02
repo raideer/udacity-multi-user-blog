b1e7af880f1a9dcc0b5324d52e779d294d5d61de
raideer <idmania1@gmail.com>
2017-07-02 16:02:10 +0300

    comments!

--- main.py
+++ main.py
@@ -22,42 +22,56 @@ import string
 import random
 from json import dumps as jsondump
 from urllib import urlencode
-
 from google.appengine.ext import db
 
+# initializing jinja template engine
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
+# secret for hashing values
 secret = 'bR>Fb397J@gM(nB\LNn*jHX'
 
+# hashes the value using our secret
 def make_secure_val(val):
     return '%s|%s' % (val, hmac.new(secret, val).hexdigest())
+
+# checks if the hash is genuine
 def check_secure_val(secure_val):
     val = secure_val.split('|')[0]
     if secure_val == make_secure_val(val):
         return val
+
+# makes a salt for hashing passwords
 def make_salt(length = 5):
     return ''.join(random.choice(string.letters) for x in xrange(length))
+
+# makes a password hash with sha256
 def make_password_hash(name, pw, salt = None):
     if not salt:
         salt = make_salt()
     h = hashlib.sha256(name + pw + salt).hexdigest()
     return '%s,%s' % (salt,h)
+
+# checks if the password hash matches with the name and password
 def check_password_hash(name, pw, h):
     salt,hash = h.split(',')
     return make_password_hash(name, pw, salt) == h
 
+# user Model
+# represents a User
 class User(db.Model):
     username = db.StringProperty(required=True)
     password = db.StringProperty(required=True)
     email = db.EmailProperty(required=False)
     created = db.DateTimeProperty(auto_now_add=True)
 
+    # finds a user with the provided name
     @classmethod
     def get_by_name(self, name):
         u = self.all().filter('username =', name).get()
         return u
 
+    # returns the user model if the user and password combination is correct
     @classmethod
     def login(self, username, password):
         u = self.get_by_name(username)
@@ -66,8 +80,11 @@ class User(db.Model):
                 return u
 
 # Required for strong consistency
+# https://cloud.google.com/datastore/docs/articles/balancing-strong-and-eventual-consistency-with-google-cloud-datastore/
 POST_ROOT = db.Key.from_path('Post', 'post_root')
 
+# post Model
+# represents a Post
 class Post(db.Model):
     subject = db.StringProperty(required=True)
     content = db.TextProperty(required=True)
@@ -76,12 +93,15 @@ class Post(db.Model):
     liked_by = db.ListProperty(db.Key)
     disliked_by = db.ListProperty(db.Key)
 
+# comment Model
+# represents a Comment in a Post
 class Comment(db.Model):
     post = db.ReferenceProperty(Post, collection_name='comments', required=True)
     author = db.ReferenceProperty(User, collection_name='comments', required=True)
     created = db.DateTimeProperty(auto_now_add=True)
     content = db.TextProperty(required=True)
 
+# default request handler
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
@@ -94,26 +114,36 @@ class Handler(webapp2.RequestHandler):
         kw['user'] = self.getCurrentUser()
         self.write(self.render_str(template, **kw))
 
+    # returns a json object instead of string/template
     def returnJson(self, object):
         self.response.headers['Content-Type'] = "application/json"
         self.response.out.write(jsondump(object))
 
+    # hashes and sets a cookie
     def setCookie(self, name, val):
         cookie_val = make_secure_val(str(val))
         self.response.headers.add_header('Set-Cookie', '%s=%s; Path=/' % (name, cookie_val))
 
+    # reads and validates a cookie
+    def readCookie(self, name):
+        cookie_val = self.request.cookies.get(name)
+        return cookie_val and check_secure_val(cookie_val)
+
+    # basic input validation
     def validUsername(self, username):
         return re.compile(r"^[a-zA-Z0-9_-]{3,20}$").match(username)
-
     def validEmail(self, email):
         return re.compile(r"^[\S]+@[\S]+.[\S]+$").match(email)
-
     def validPassword(self, password):
         return re.compile(r"^.{3,20}$").match(password)
 
+    # checks if the username is not taken
     def uniqueUsername(self, username):
         return not User.get_by_name(username)
 
+    # returns id and name of the current logged in user
+    # if return_model is True
+    #   user Model is returned instead
     def getCurrentUser(self, return_model = False):
         user_id = self.readCookie('user_id')
         user = self.readCookie('user')
@@ -125,20 +155,19 @@ class Handler(webapp2.RequestHandler):
             else:
                 return current_user
 
+    # set user cookies if the login was successful
     def login(self, user):
         self.setCookie('user_id', str(user.key().id()))
         self.setCookie('user', user.username)
 
+    # clears all cookies responsible for user authorization
     def logout(self):
         self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
         self.response.headers.add_header('Set-Cookie', 'user=; Path=/')
 
-    def readCookie(self, name):
-        cookie_val = self.request.cookies.get(name)
-        return cookie_val and check_secure_val(cookie_val)
-
-# For pages that require user to be logged in
+# Request handler for pages that require the user to be logged IN
 class AuthorizedHandler(Handler):
+    # Dispatch is triggered when any of the http methods are called
     def dispatch(self):
         user = self.getCurrentUser()
 
@@ -147,13 +176,15 @@ class AuthorizedHandler(Handler):
             return self.redirect('/user/login')
         super(AuthorizedHandler, self).dispatch()
 
-# For pages that require user to be logged out
+# Request handler for pages that require the user to be logged OUT
 class UnauthorizedHandler(Handler):
     def dispatch(self):
         if self.getCurrentUser():
             return self.redirect('/user/profile')
         super(UnauthorizedHandler, self).dispatch()
 
+# User registration handler
+# Accessible only by guests
 class UserRegister(UnauthorizedHandler):
     def get(self):
         self.render('signup/signup.html', errors = {})
@@ -165,6 +196,7 @@ class UserRegister(UnauthorizedHandler):
 
         errors = {}
 
+        # input validation
         if not self.validUsername(username):
             errors['username'] = "That's not a valid username"
         if not username:
@@ -182,9 +214,11 @@ class UserRegister(UnauthorizedHandler):
         if password != verify:
             errors['verify'] = "Your passwords didn't match"
 
+        # show errors if there are any
         if len(errors) > 0 :
             return self.render('signup/signup.html', errors = errors, username = username, email = email)
         else:
+            # else complete the registration
             pwhash = make_password_hash(username, password)
             user = User(username = username, password = pwhash)
             if email:
@@ -194,11 +228,15 @@ class UserRegister(UnauthorizedHandler):
             self.login(user)
             self.redirect('/user/profile')
 
+# Logout handler
+# Accessible only by users that are logged in
 class UserLogout(AuthorizedHandler):
     def get(self):
         self.logout()
         self.redirect('/user/login')
 
+# Login handler
+# Accessible only by users that are logged out
 class UserLogin(UnauthorizedHandler):
     def get(self):
         self.render('signup/login.html', error = False)
@@ -207,31 +245,44 @@ class UserLogin(UnauthorizedHandler):
         password = self.request.get('password')
 
         user = User.login(username, password)
+        # if the username/password combination is incorrect
+        # return to the login page
         if not user:
             return self.render('signup/login.html', error = True)
         else:
             self.login(user)
             self.redirect('/user/profile')
 
+# User profile handler
+# Accessible only by users that are logged in
 class UserProfile(AuthorizedHandler):
     def get(self):
         u = self.getCurrentUser(return_model = True)
         self.render('signup/profile.html', u = u)
 
+# Blog handler
 class Blog(Handler):
     def get(self):
+        # Returns all posts ordered by 'created' descending
+        # ancestor method makes sure that the posts we see are up to date
+        #
+        # otherwise if you delete a post, it will not disappear immediately
+        # after page refresh
         posts = Post.all().order('-created').ancestor(POST_ROOT)
         self.render('blog/index.html', posts = posts)
 
+# Blog post handler
 class BlogPost(Handler):
     def get(self, postId):
         post = Post.get_by_id(int(postId), POST_ROOT)
         self.render('blog/post.html', post = post)
 
+# Handles post deletion
 class BlogDeletePost(AuthorizedHandler):
     def get(self, postId):
         post = Post.get_by_id(int(postId), POST_ROOT)
         user = self.getCurrentUser(return_model = True)
+        # Checks if the user is author of the post
         if post.author.key() == user.key():
             self.render('blog/deletepost.html', post = post)
         else:
@@ -239,15 +290,20 @@ class BlogDeletePost(AuthorizedHandler):
     def post(self, postId):
         post = Post.get_by_id(int(postId), POST_ROOT)
         user = self.getCurrentUser(return_model = True)
-        if post.author.key().id() == user.key().id():
+        # Rechecks if the user is author of the post
+        if post.author.key() == user.key():
             post.delete()
         self.redirect('/')
 
+# Handles post upvoting/downvoting
 class BlogRatePost(AuthorizedHandler):
+    # see routing to understand where postId and action comes from
+    # action can either be 'upvote' or 'downvote'
     def post(self, postId, action):
         post = Post.get_by_id(int(postId), POST_ROOT)
         user = self.getCurrentUser(return_model = True)
 
+        # Check if user is not the author of the post
         if post.author.key() != user.key():
             # If user hasn't voted yet
             if user.key() not in post.liked_by and user.key() not in post.disliked_by:
@@ -269,22 +325,32 @@ class BlogRatePost(AuthorizedHandler):
             post.put()
 
             votes = len(post.liked_by) - len(post.disliked_by)
+
+            # since this request is made via ajax post request (to avoid refreshing),
+            # we are returning json data
+            #
+            # see the bottom of ./template.html for more information
             self.returnJson({'votes': votes})
         else:
             self.returnJson({'error': "You can't rate your own posts!"})
 
+# Handles commenting
 class BlogCommentPost(AuthorizedHandler):
     def post(self, postId):
         post = Post.get_by_id(int(postId), POST_ROOT)
         user = self.getCurrentUser(return_model = True)
         content = self.request.get('content')
+        # makes sure the user is logged in
         if user:
             comment = Comment(parent=post.key(), post=post, author=user, content=content)
             comment.put()
             self.redirect('/post/' + str(post.key().id()))
 
+# Handles comment deletion
 class BlogDeleteComment(AuthorizedHandler):
     def post(self):
+        # creating a database Key
+        # required for strong consistency
         postKey = db.Key(self.request.get('post_key'))
         commentId = self.request.get('comment_id')
         user = self.getCurrentUser(return_model = True)
@@ -295,6 +361,7 @@ class BlogDeleteComment(AuthorizedHandler):
                 comment.delete()
         self.redirect('/post/'+str(postKey.id()))
 
+# Handles comment editing
 class BlogEditComment(AuthorizedHandler):
     def post(self):
         postKey = db.Key(self.request.get('post_key'))
@@ -309,10 +376,12 @@ class BlogEditComment(AuthorizedHandler):
                 comment.put()
         self.redirect('/post/'+str(postKey.id()))
 
+# Handles post editing
 class BlogEditPost(AuthorizedHandler):
     def get(self, postId):
         post = Post.get_by_id(int(postId), POST_ROOT)
         user = self.getCurrentUser(return_model = True)
+        # check if user is author of the post
         if post.author.key() == user.key():
             self.render('blog/editpost.html', post = post)
         else:
@@ -323,12 +392,16 @@ class BlogEditPost(AuthorizedHandler):
 
         post = Post.get_by_id(int(postId), POST_ROOT)
 
+        # check if the post actually exists
         if not post:
             return self.redirect('/')
 
         user = self.getCurrentUser(return_model = True)
 
+        # cehck if user is author of the post
         if post.author.key() == user.key():
+            # making sure subject and content inputs exist before
+            # we overwrite the post
             if subject and content:
                 post.subject = subject
                 post.content = content
@@ -336,11 +409,12 @@ class BlogEditPost(AuthorizedHandler):
 
                 self.redirect('/post/' + str(post.key().id()))
             else:
-                error = "Title and content is REQUIRED"
-                self.render('blog/editpost.html', post = post)
+                error = 'Subject and content is REQUIRED'
+                self.render('blog/editpost.html', post = post, error = error)
         else:
             return self.write("You can only edit your own posts!")
 
+# Handles post creation
 class BlogNewPost(AuthorizedHandler):
     def get(self):
         self.render('blog/newpost.html')
@@ -358,6 +432,7 @@ class BlogNewPost(AuthorizedHandler):
             error = "Title and content is REQUIRED"
             self.render('blog/newpost.html', error = error, subject = subject, content = content)
 
+# Routing
 app = webapp2.WSGIApplication([
     ('/user/signup', UserRegister),
     ('/user/profile', UserProfile),
